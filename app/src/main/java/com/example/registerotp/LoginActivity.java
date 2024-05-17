c328e4118cb223f8146122b608e385770c27c231
Alex Pavlenko <alepavlenko@mail.ru>
2024-05-17 08:35:00 +0200

    StartPage

--- app/src/main/java/com/example/registerotp/LoginActivity.java
+++ app/src/main/java/com/example/registerotp/LoginActivity.java
@@ -7,16 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+/*
+* Aktiviert die Edge-to-Edge-Darstellung und stellt sicher, dass der Inhalt der Aktivität
+* nicht von den Systemleisten verdeckt wird, indem er passende Abstände setzt!
+* */
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        //in activity_login definierte Layout.
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            //wenn Fenster-Insets (z.B. Statusleiste, Navigationsleiste) angewendet werden.
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
--- app/src/main/java/com/example/registerotp/SplashActivity.java
+++ app/src/main/java/com/example/registerotp/SplashActivity.java
@@ -1,10 +1,17 @@
 package com.example.registerotp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.registerotp.utils.FirebaseUtil;
 
@@ -15,15 +22,31 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        int color_left = ContextCompat.getColor(this, R.color.logo_tet_left);
+        int color_right = ContextCompat.getColor(this, R.color.logo_tet_right);
+
+        TextView logo_text = findViewById(R.id.logo_text);
+        String get_logo_text = getString(R.string.app_name_s);
+
+        SpannableString spannableString = new SpannableString(get_logo_text);
+        spannableString.setSpan(new ForegroundColorSpan(color_left),0,4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(color_right), get_logo_text.length() - 3, get_logo_text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        logo_text.setText(spannableString);
+
+
+
 
         new Handler().postDelayed(() -> {
+            //Prüft, ob der Benutzer eingeloggt ist
             if (FirebaseUtil.isLoggedIn()) {
+                // Wenn der Benutzer eingeloggt ist, startet die MainActivity
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            // Wenn der Benutzer nicht eingeloggt ist, startet die LoginActivity
             } else {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             }
             finish();
-        }, 1000);
+        }, 3000);
     }
-
 }
