d7543079c0c846c5a0652f14a36a823de3bc3cd4
Alex Pavlenko <alepavlenko@mail.ru>
2024-05-27 00:22:17 +0200

    StartPage

--- app/src/main/java/com/example/registerotp/LoginActivity.java
+++ app/src/main/java/com/example/registerotp/LoginActivity.java
@@ -1,30 +1,48 @@
 package com.example.registerotp;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
+
 /*
 * Aktiviert die Edge-to-Edge-Darstellung und stellt sicher, dass der Inhalt der Aktivität
 * nicht von den Systemleisten verdeckt wird, indem er passende Abstände setzt!
 * */
 public class LoginActivity extends AppCompatActivity {
+    private LinearLayout mainLayout;
+    private TextInputLayout txtUsername;
+    private TextInputLayout txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mainLayout = findViewById(R.id.main);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+
+
         EdgeToEdge.enable(this);
         //in activity_login definierte Layout.
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        //setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_login);
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             //wenn Fenster-Insets (z.B. Statusleiste, Navigationsleiste) angewendet werden.
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
+
+
+
     }
 }
--- app/src/main/java/com/example/registerotp/SplashActivity.java
+++ app/src/main/java/com/example/registerotp/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.registerotp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -8,6 +10,10 @@ import android.os.Handler;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,31 +28,29 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        int color_left = ContextCompat.getColor(this, R.color.logo_tet_left);
-        int color_right = ContextCompat.getColor(this, R.color.logo_tet_right);
+        ImageView sichtLogoText = findViewById(R.id.splasch_text_logo);
 
-        TextView logo_text = findViewById(R.id.logo_text);
-        String get_logo_text = getString(R.string.app_name_s);
-
-        SpannableString spannableString = new SpannableString(get_logo_text);
-        spannableString.setSpan(new ForegroundColorSpan(color_left),0,4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(color_right), get_logo_text.length() - 3, get_logo_text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        logo_text.setText(spannableString);
-
-
-
-
-        new Handler().postDelayed(() -> {
-            //Prüft, ob der Benutzer eingeloggt ist
-            if (FirebaseUtil.isLoggedIn()) {
-                // Wenn der Benutzer eingeloggt ist, startet die MainActivity
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            // Wenn der Benutzer nicht eingeloggt ist, startet die LoginActivity
-            } else {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        sichtLogoText.setAlpha(0f);
+        sichtLogoText.setVisibility(View.VISIBLE);
+        sichtLogoText.animate().alpha(1f).setDuration(2500).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // Handler
+                new Handler().postDelayed(() -> {
+                    //Prüft, ob der Benutzer eingeloggt ist
+                    if (FirebaseUtil.isLoggedIn()) {
+                        // Wenn der Benutzer eingeloggt ist, startet die MainActivity
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        // Wenn der Benutzer nicht eingeloggt ist, startet die LoginActivity
+                    } else {
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    }
+                    finish();
+                }, 1000);
             }
-            finish();
-        }, 3000);
+        });
+
+
+
     }
 }
